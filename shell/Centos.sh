b066f85c655c8118b3f713975e2645a5cbdc9a6d
suches <908202921@qq.com>
2018-12-17 22:22:15 +0800

    up

--- shell/Centos.sh
+++ shell/Centos.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 echo 
-echo 'initing tippecanoe'
+echo 'initing tippecanoe ( centos )'
 echo 
 echo 'please waiting a moment...'
 echo 
-#sudo /usr/bin/apt-get update && echo 
-#sudo /usr/bin/apt-get -y upgrade && echo 
-#sudo /usr/bin/apt-get -y install git build-essential libsqlite3-dev zlib1g-dev tree && echo 
-#sudo git clone https://github.com/mapbox/tippecanoe.git ~/tippecanoe && echo 
-#sudo make -j -C ~/tippecanoe && echo 
-#sudo make -C ~/tippecanoe install && echo 
-#sudo rm -rf ~/tippecanoe
-#sudo add-apt-repository ppa:ubuntugis/ppa && echo 
-#sudo sudo apt-get update && echo 
-#sudo apt-get install gdal-bin && echo  
+if which sudo >/dev/null; then
+    sudo yum install -y epel-release && echo 
+    sudo yum update -y && echo 
+    sudo yum group install -y "Development Tools" && echo 
+    sudo yum install -y wget gzip tree && echo 
+    sudo yum install -y gdal-devel && echo 
+    sudo yum install -y libsqlite3x-devel zlib zlib-devel && echo 
+    git clone https://github.com/mapbox/tippecanoe.git ~/tippecanoe && echo 
+    sudo make -j -C ~/tippecanoe && echo 
+    sudo make -C ~/tippecanoe install && echo 
+    sudo rm -rf ~/tippecanoe && echo 
+
+else
+    yum install -y epel-release && echo 
+    yum update -y && echo 
+    yum group install -y "Development Tools" && echo 
+    yum install -y wget gzip tree && echo 
+    yum install -y gdal-devel && echo 
+    yum install -y libsqlite3x-devel zlib zlib-devel && echo 
+    git clone https://github.com/mapbox/tippecanoe.git ~/tippecanoe && echo 
+    make -j -C ~/tippecanoe && echo 
+    make -C ~/tippecanoe install && echo 
+    rm -rf ~/tippecanoe && echo 
+fi
+
 echo  
 echo "tippecanoe enviorment init done"
--- shell/Ubuntu.sh
+++ shell/Ubuntu.sh
@@ -4,17 +4,33 @@ echo 'initing tippecanoe'
 echo 
 echo 'please waiting a moment...'
 echo 
-sudo add-apt-repository ppa:ubuntugis/ppa -y && echo 
-sudo apt-get update && echo 
-sudo apt-get -y upgrade && echo 
 
-sudo apt-get -y install git build-essential libsqlite3-dev zlib1g-dev tree gzip && echo 
-sudo git clone https://github.com/mapbox/tippecanoe.git ./tippecanoe && echo 
-sudo make -j -C ./tippecanoe && echo 
-sudo make -C ./tippecanoe install && echo 
-sudo rm -rf ./tippecanoe
+if which sudo >/dev/null; then
+    sudo add-apt-repository ppa:ubuntugis/ppa -y && echo 
+    sudo apt-get update && echo 
+    sudo apt-get -y upgrade && echo 
 
-sudo apt-get install gdal-bin -y && echo  
+    sudo apt-get -y install git build-essential libsqlite3-dev zlib1g-dev tree gzip && echo 
+    sudo git clone https://github.com/mapbox/tippecanoe.git ~/tippecanoe && echo 
+    sudo make -j -C ~/tippecanoe && echo 
+    sudo make -C ~/tippecanoe install && echo 
+    sudo rm -rf ~/tippecanoe
+
+    sudo apt-get install gdal-bin -y && echo  
+else
+    add-apt-repository ppa:ubuntugis/ppa -y && echo 
+    apt-get update && echo 
+    apt-get -y upgrade && echo 
+
+    apt-get -y install git build-essential libsqlite3-dev zlib1g-dev tree gzip && echo 
+    git clone https://github.com/mapbox/tippecanoe.git ~/tippecanoe && echo 
+    make -j -C ~/tippecanoe && echo 
+    make -C ~/tippecanoe install && echo 
+    rm -rf ~/tippecanoe
+
+    apt-get install gdal-bin -y && echo 
+fi
+
 
 echo  
 echo "tippecanoe enviorment init done"
